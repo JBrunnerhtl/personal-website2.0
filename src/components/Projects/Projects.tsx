4fc0958012af42f42b816bc3ef909a211f4a6e17
JBrunnerhtl <j.brunner1@students.htl-leonding.ac.at>
2026-01-15 18:36:12 +0100

    added new project

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -8,7 +8,8 @@ function Projects()
         {urlLinkMd:"https://raw.githubusercontent.com/JBrunnerhtl/RpnCalculator/main/README.md", repoName:"Rpn Calculator", repoUrl:"https://github.com/JBrunnerhtl/RpnCalculator"},
         {urlLinkMd:"https://raw.githubusercontent.com/JBrunnerhtl/Project-Fitness-and-Health/main/README.md", repoName:"Project Fitness and Health", repoUrl:"https://github.com/JBrunnerhtl/Project-Fitness-and-Health"},
         {urlLinkMd:"https://raw.githubusercontent.com/JBrunnerhtl/Todo-List/main/README.md", repoName:"JavaScript Todo List", repoUrl:"https://github.com/JBrunnerhtl/Todo-List"},
-        {urlLinkMd: "https://raw.githubusercontent.com/JBrunnerhtl/personal-website2.0/main/README.md", repoName:"Personal Website 2.0", repoUrl:"https://github.com/JBrunnerhtl/personal-website2.0?tab=readme-ov-file"}
+        {urlLinkMd: "https://raw.githubusercontent.com/JBrunnerhtl/personal-website2.0/main/README.md", repoName:"Personal Website 2.0", repoUrl:"https://github.com/JBrunnerhtl/personal-website2.0?tab=readme-ov-file"},
+        {urlLinkMd: "https://raw.githubusercontent.com/JBrunnerhtl/Leetcode/main/README.md", repoName: "Leetcode", repoUrl:"https://github.com/JBrunnerhtl/Leetcode"}
     ]
     const cards = input.map((item, index) => ( <RepoCard urlLinkMd={item.urlLinkMd} repoName={item.repoName} repoUrl={item.repoUrl} key={index}/>));
 
